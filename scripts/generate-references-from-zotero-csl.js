9a4bf4460a9537c089b574bca01c9940419a1b33
Dirk Rathje <post@dirk-rathje.de>
2023-03-05 05:39:48 +0100

    ...

--- scripts/generate-references-from-zotero-csl.js
+++ scripts/generate-references-from-zotero-csl.js
@@ -69,7 +69,7 @@ const generate = ({ globPattern, tagScoper }) => {
         'reference',
         `${bibliographyEntry.cslItem.id}.md`
       ),
-      `---\n${dump(bibliographyEntry)}\n---\n`
+      `---\n${dump(bibliographyEntry, { quotingType: '"' })}---\n`
     )
   })
 
